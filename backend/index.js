dcfe8d0b5e68885aeb06b33a8edf7f338a114a20
Hussein Mansour <husseinmansour@Husseins-MacBook-Air.local>
2019-05-13 20:10:16 -0400

    test static 1

--- backend/index.js
+++ backend/index.js
@@ -320,6 +320,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
 })
 
+
+
 app.listen(80, function() {
   //change it to 80
   console.log("Server started on port 80")
